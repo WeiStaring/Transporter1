f7e94aecdbf63541ce95ba0fe6ce18f4ad8ce889
kirito0524 <571843783@qq.com>
2019-04-19 20:05:01 +0800

    test

--- cloudfunction/road/index.js
+++ cloudfunction/road/index.js
@@ -1,6 +1,8 @@
 'use strict';
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
 exports.main = (event, context, callback) => {
-    console.log("Hello World")
+    console.log("Hello World123")
     console.log(event)
     console.log(event["non-exist"])
     console.log(context)
